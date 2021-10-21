30beb7d6a2bfc9ad544a9e5cc923f4a2e2972620
Shiv <shiv.stn98@gmail.com>
2021-10-21 13:53:25 +0530

    some changes

--- src/components/AccoutDetailsCard/AccoutDetailsCard.js
+++ src/components/AccoutDetailsCard/AccoutDetailsCard.js
@@ -5,20 +5,38 @@ import angledArrow from "../../assets/icons/angled-arrow.svg";
 
 const AccoutDetailsCard = () => {
   return (
-    <div className="acc-details">
-      <div className="acc-details-icon">
-        <img src={acIcon} alt="ac-icon" />
-      </div>
-      <div className="acc-details-title">Saving account</div>
-
-      <div className="acc-details-data">₹ 23,49,422</div>
-
-      <div className="acc-details-meta">
-        <div className="left">
-          <img src={angledArrow} alt="angledArrow" />
-          <span>₹4500</span>
+    <div className="acc-details-container">
+      <div className="acc-details">
+        <div className="acc-details-icon">
+          <img src={acIcon} alt="ac-icon" />
         </div>
-        <div className="right">20 min ago</div>
+        <div className="acc-details-title">Saving account</div>
+
+        <div className="acc-details-data">₹ 23,49,422</div>
+
+        <div className="acc-details-meta">
+          <div className="left">
+            <img src={angledArrow} alt="angledArrow" />
+            <span>₹4500</span>
+          </div>
+          <div className="right">20 min ago</div>
+        </div>
+      </div>
+      <div className="acc-details">
+        <div className="acc-details-icon">
+          <img src={acIcon} alt="ac-icon" />
+        </div>
+        <div className="acc-details-title">Saving account</div>
+
+        <div className="acc-details-data">₹ 23,49,422</div>
+
+        <div className="acc-details-meta">
+          <div className="left">
+            <img src={angledArrow} alt="angledArrow" />
+            <span>₹4500</span>
+          </div>
+          <div className="right">20 min ago</div>
+        </div>
       </div>
     </div>
   );
