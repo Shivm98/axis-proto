5a220173a5e8f512f5f9c793788cf3778396e32f
Shiv <shiv.stn98@gmail.com>
2021-10-25 12:48:07 +0530

    added minimize feature

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./Header.scss";
 import minimize from "../../assets/icons/minimize.svg";
 
-const Header = () => {
+const Header = ({ miniMizeBot }) => {
   return (
     <div className="header">
       <div className="logo"></div>
       <div className="title">Axis AHA!</div>
-      <div className="icon">
+      <div className="icon" onClick={miniMizeBot}>
         <img src={minimize} alt="minimize" />
       </div>
     </div>
--- src/layout/Chatbot/Chatbot.js
+++ src/layout/Chatbot/Chatbot.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 import Body from "../../components/Body/Body";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import "./Chatbot.scss";
 
 const Chatbot = () => {
+  const [mini, setMini] = useState(false);
+
+  const miniMizeBot = () => {
+    setMini((prev) => !mini);
+  };
+
   return (
-    <div className="chatbot">
+    <div className={`${mini ? "mini chatbot" : "chatbot"}`}>
       <div className="header-container">
-        <Header />
+        <Header miniMizeBot={miniMizeBot} />
       </div>
       <div className="body-container">
         <Body />
